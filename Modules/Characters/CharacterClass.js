73eab1e590476abd8361b778961a424cfc576856
Brian <hayesb20@hanover.edu>
2019-09-16 15:49:30 -0400

    Created the Character Class constructor and getter functions

--- Modules/Characters/CharacterClass.js
+++ Modules/Characters/CharacterClass.js
@@ -1,1 +1,21 @@
 //CharacterClass.js
+
+export default class Character{
+	constructor(String name, String race, Int level = 1, Int experiance = 0){
+		this.name		= name;
+		this.level		= level;
+		this.race		= census_LookUp(race);
+		this.attributes = this.race.getAttributes(level);
+		this.experiance = experiance;
+	}
+
+	get_Name(){return this.name;}
+
+	get_Level(){return this.level;}
+
+	getRace(){return this.race;}
+
+	get_Attributes(){return this.attributes;}
+
+	get_Experiance(){return this.experiance;}
+}
