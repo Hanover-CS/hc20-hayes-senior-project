e4e2439995133cae76b8433ce7e209e641b0d233
Brian <hayesb20@hanover.edu>
2019-10-17 10:56:18 -0400

    working with HTML

---
+++ Controllers/CharacterCreate.js
@@ -0,0 +1,11 @@
+import logthis from "../Modules/Characters/CharacterClass.js"
+
+let list = [];
+
+let submitButton = document.getElementById("submitButton");
+
+submitButton.onclick = function(){
+    // logthis();
+    console.log("hello")
+}
+
