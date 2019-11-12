789dda565089449fc1027fb062c8d5a54fd56953
Brian <hayesb20@hanover.edu>
2019-11-12 11:28:43 -0500

    Clean up
    Cleaned the File up and made the previously exported functions, class methods.

--- Modules/Characters/Non-Playable Characters/Skeleton.js
+++ Modules/Characters/Non-Playable Characters/Skeleton.js
@@ -1,38 +1,39 @@
 
-class Skeleton {
-	constructor(){
+export default class Skeleton {
+	constructor() {
 		this.attributes = {
-			might:55,
-			projection:0,
-			vitality:55,
-			fortitude:2,
-			agility:125,
-			intelligence:0,
-			willpower:0,
-			intimidation:2,
+			might: 55,
+			projection: 0,
+			vitality: 55,
+			fortitude: 2,
+			agility: 125,
+			intelligence: 0,
+			willpower: 0,
+			intimidation: 2,
 		}
 	}
-	get_All_Attributes(){
+
+	get_All_Attributes() {
 		let copy_Of_Attributes = Object.assign({}, this.attributes)
 		return copy_Of_Attributes;
+	}
+
+	getSkeleton() { return skeleton; }
+
+	levelUpGuide() {
+		let levelUp_Guide = {
+			might: 1,
+			projection: 0,
+			vitality: 10,
+			intelligence: 0,
+			willpower: 0,
+			agility: 0,
+			fortitude: 0,
+			intimidation: 0
+		};
+		return levelUp_Guide;
 	}
 }
 
 
 const skeleton = new Skeleton();
-
-export function get_Race_From_String_From_Skeleton_Class(race){
-
-	if(race === "Skeleton"){
-		return skeleton;
-	}
-}
-
-export function get_The_levelUp_Guide_For_Skeletons(race){
-
-	if(race === "Skeleton"){
-		let levelUp_Guide = {might:1, projection:0, vitality:10, intelligence:0,
-			willpower:0 , agility:0 , fortitude:0 , intimidation:0};
-		return levelUp_Guide;
-    }
-}
