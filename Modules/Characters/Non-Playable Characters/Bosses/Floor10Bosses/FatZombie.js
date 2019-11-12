81cc3a9bbae2c03bb08989e009b3018ffa525b60
Brian <hayesb20@hanover.edu>
2019-11-12 11:24:16 -0500

    CleanUp of the fat zombie class
    Cleaned the File up and made the previously exported functions, class methods.

--- Modules/Characters/Non-Playable Characters/Bosses/Floor10Bosses/FatZombie.js
+++ Modules/Characters/Non-Playable Characters/Bosses/Floor10Bosses/FatZombie.js
@@ -1,27 +1,21 @@
-class FatZombie {
-	constructor(){
+export default class FatZombie {
+	constructor() {
 		this.attributes = {
-			might:5,
-			projection:0,
-			vitality:50,
-			fortitude:2,
-			agility:5,
-			intelligence:0,
-			willpower:0,
-			intimidation:2,
+			might: 5,
+			projection: 0,
+			vitality: 50,
+			fortitude: 2,
+			agility: 5,
+			intelligence: 0,
+			willpower: 0,
+			intimidation: 2,
 		}
 	}
-	get_All_Attributes(){
+	get_All_Attributes() {
 		let copy_Of_Attributes = Object.assign({}, this.attributes)
 		return copy_Of_Attributes;
 	}
+	getFatZombie() { return fat_Zombie; }
 }
 
 const fat_Zombie = new FatZombie();
-
-export function get_Race_From_String_From_FatZombie_Class(race){
-
-	if(race === "Fat Zombie"){
-		return fat_Zombie;
-	}
-}
