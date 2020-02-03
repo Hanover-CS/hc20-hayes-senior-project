851cc06b03ac8788eace76ec9c8f195d0af8959f
Brian <hayesb20@hanover.edu>
2020-02-03 15:20:38 -0500

    Ref #58 Ref #60
    Balancing the prices of items,
    Added descriptions, 
    and formatted the code

--- Modules/Item/ItemCatalogClass.js
+++ Modules/Item/ItemCatalogClass.js
@@ -17,14 +17,22 @@ export default class ItemCatalog {
 }
 
 let catalog = {
-    "Rayashk": new Item("Rayashk", 350, "Ledgendary Sword that was give as the prize in the legendary tournimate of kings", "Weapon", { attack: 20, vitality: 0, fortitude: 0 }),
-    "Worlds End": new Item("Worlds End", 1000, "Mythic Sword", "Weapon", { attack: 20, vitality: 0, fortitude: 0 }),
+    // Weapons
+    "Rayashk": new Item("Rayashk", 500, "Ledgendary Sword that was give as the prize" +
+         "in the legendary tournimate of kings", "Weapon", { attack: 20, vitality: 0, fortitude: 0 }),
+    "Worlds End": new Item("Worlds End", 5000, "Mythic Sword", "Weapon", { attack: 200, vitality: 0, fortitude: 0 }),
 
-    // Legendary Gear Set - Fire
-    "Legendary Fire head": new Item("Legendary Fire head", 1000, "description", "Head", { attack: 0, vitality: 20, fortitude: 0 }),
-    "Legendary Fire torso": new Item("Legendary Fire torso", 1000, "description", "Torso", { attack: 0, vitality: 20, fortitude: 0 }),
-    "Legendary Fire hands": new Item("Legendary Fire hands", 1000, "description", "Hands"),
-    "Legendary Fire waist": new Item("Legendary Fire waist", 1000, "description", "Waist"),
-    "Legendary Fire legs": new Item("Legendary Fire legs", 1000, "description", "Legs"),
-    "Legendary Fire feet": new Item("Legendary Fire feet", 1000, "description", "Feet")
+    // Starter Gear Set
+    "Thronian Helm": new Item("The Helmets Worn by the Royal Thronian Guard.", 1500, "description", "Head", 
+        { attack: 0, vitality: 10, fortitude: 5 }),
+    "Thronian ChestGuard": new Item("The ChestGuards Worn by the Royal Thronian Guard.", 1500, "description", "Torso", 
+        { attack: 0, vitality: 20, fortitude: 10 }),
+    "Thronian Gauntlets": new Item("The Gauntlets Worn by the Royal Thronian Guard.", 1500, "description", "Hands", 
+        { attack: 5, vitality: 0, fortitude: 3 }),
+    "Thronian Belt": new Item("The Belts Worn by the Royal Thronian Guard.", 1500, "description", "Waist", 
+        { attack: 0, vitality: 5, fortitude: 6 }),
+    "Thronian Pants": new Item("The Pants Worn by the Royal Thronian Guard.", 1500, "description", "Legs", 
+        { attack: 0, vitality: 20, fortitude: 10 }),
+    "Thronian Boots": new Item("The Boots Worn by the Royal Thronian Guard.", 1500, "description", "Feet", 
+        { attack: 0, vitality: 5, fortitude: 5 })
 }
